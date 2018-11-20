1a8f47c5ee1bf821783ae8813e27b018755ac72c
toorealy <36117535+toorealy@users.noreply.github.com>
2018-11-20 16:11:54 -0600

    Add files via upload

---
+++ Copy File to Remote Host.ps1
@@ -0,0 +1,26 @@
+﻿function Copy-TargetFile {
+    Add-Type -AssemblyName System.Windows.Forms
+    $fileToCopy = New-Object System.Windows.Forms.OpenFileDialog
+    $fileToCopy.ShowDialog()
+    
+    $a=$fileToCopy.FileName
+
+    Copy-Item  @params{
+        Path = $a 
+        ToSession = $session 
+        Destination = 'C:\windows\temp\'$fileToCopy.SafeFileName
+        }
+        
+}
+
+
+function Open-CopySession {
+    $targetComputer = Read-Host -Prompt 'What is the HOSTNAME of the target?' 
+
+    # Create a session with $targetComputer
+        $session = New-PSSession -ComputerName $targetComputer
+
+
+}
+Open-CopySession
+Copy-TargetFile
